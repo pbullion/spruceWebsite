4b23c8dce9729e87a9c061c13b35501e11024f9a
pbullion <pbullion@gmail.com>
2019-07-18 16:42:28 -0500

    updates

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,10 +3,10 @@ import { Container, Row, Col } from "react-bootstrap";
 import { withRouter, Link } from "react-router-dom";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
-// import "./HomePage.css";
+import axios from "axios";
+import { SocialIcon } from 'react-social-icons';
 import spruce1 from "../../assets/carouselPics/spruce1.jpg";
 import spruce2 from "../../assets/carouselPics/spruce2.jpg";
-import special from "../../assets/carouselPics/special.jpg";
 import logo from "../../assets/logos/spruceLogo.png";
 import appStore from "../../assets/appstore.svg";
 
@@ -14,13 +14,36 @@ const centerStyle = {
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
-  justifyContent: "center"
+  justifyContent: "center",
+  width: "100vw"
 };
 
 class HomePage extends Component {
-  state = {};
+  state = {
+    businessHours: null,
+    specials: null
+  };
 
-  componentWillMount() {}
+  _getBusinessHours = () => {
+    axios
+      .get(`http://18.237.192.82:3001/homeScreen/businessHours`)
+      .then(res => {
+        const businessHours = res.data;
+        this.setState({ businessHours });
+      });
+  };
+
+  _getSpecials = () => {
+    axios.get(`http://18.237.192.82:3001/homeScreen/specials`).then(res => {
+      const specials = res.data;
+      this.setState({ specials });
+    });
+  };
+
+  componentWillMount() {
+    this._getBusinessHours();
+    this._getSpecials();
+  }
 
   render() {
     return (
@@ -47,7 +70,11 @@ class HomePage extends Component {
           </Carousel>
         </Row>
         <Row>
-          <img src={appStore} style={{ marginTop: 10 }} alt="home screen" />
+          <img
+            src={appStore}
+            style={{ marginTop: 10, marginBottom: 10 }}
+            alt="app store icon"
+          />
         </Row>
         <Row>
           <Link to={"/waitlist"}>
@@ -55,10 +82,48 @@ class HomePage extends Component {
           </Link>
         </Row>
         <Row>
-          <h3>Business Hours</h3>
+          <SocialIcon url="https://www.facebook.com/sprucebarbershop/" bgColor="#2F553C" fgColor="#ffffff"style={{margin: 10}}/>
+          <SocialIcon url="https://www.instagram.com/sprucebarbershop/" bgColor="#2F553C" fgColor="#ffffff"style={{margin: 10}}/>
         </Row>
-        <Row>
-          <h3>Specials</h3>
+        <Row style={centerStyle}>
+          <h3 style={{ marginBottom: 10 }}>Business Hours</h3>
+          {this.state.businessHours
+            ? this.state.businessHours.map((item, index) => {
+                return (
+                  <Row
+                    style={{ width: "100vw", textAlign: "center" }}
+                    key={index}
+                  >
+                    <Col xs={6}>
+                      <p>{item.day}</p>
+                    </Col>
+                    <Col xs={6}>
+                      <p>{item.hours}</p>
+                    </Col>
+                  </Row>
+                );
+              })
+            : null}
+        </Row>
+        <Row style={centerStyle}>
+          <h3 style={{ marginBottom: 10 }}>Specials</h3>
+          {this.state.specials
+            ? this.state.specials.map((item, index) => {
+                return (
+                  <Row
+                    style={{ width: "100vw", textAlign: "center" }}
+                    key={index}
+                  >
+                    <Col xs={6}>
+                      <p>{item.type}</p>
+                    </Col>
+                    <Col xs={6}>
+                      <p>{item.special}</p>
+                    </Col>
+                  </Row>
+                );
+              })
+            : null}
         </Row>
       </Container>
     );
